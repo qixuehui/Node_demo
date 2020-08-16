5958cb9ad6afbce3d1730f34fe43a64819622659
qxh <3048086150@qq.com>
2020-08-16 16:00:58 +0800

    node blog start

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # NodeJs
-
-# feedback
-
---
+++ manyblog/app.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var path = require('path')
+
+var app = express()
+    //公开资源
+app.use('/public/', express.static(path.join(__dirname, './public/')))
+app.use('/node_modules', express.static(path.join(__dirname, './node_modules/')))
+    //中间件
+app.engine('html', require('express-art-template'))
+
+
+app.get('/', function(req, res) {
+    res.send('hello')
+
+})
+
+app.listen(8080, function() {
+    console.log('running~~~');
+})
