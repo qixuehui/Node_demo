6b28d858c25bf497122ad055130883e33a47cf84
qxh <3048086150@qq.com>
2020-08-18 20:00:59 +0800

    用户的资料显示

--- manyblog/routes/user.js
+++ manyblog/routes/user.js
@@ -11,25 +11,35 @@ router.get('/settings/admin', function(req, res) {
     })
     //个人资料
 router.post('/settings/admin', function(req, res) {
-    //修改其
+    //修改其密码
     var body = req.body
 })
 
 router.get('/settings/profile', function(req, res) {
-    res.render('settings/profile.html', {
-        user: req.session.user
+        res.render('settings/profile.html', {
+            user: req.session.user
+        })
     })
+    //修改其资料
+router.post('/settings/profile', function(req, res) {
+
 })
 
 router.get("/settings/delete", async(req, res) => {
+    //删除成功
     try {
-        await user.deleteOne({ email: user.session.email })
+        await user.deleteOne({ email: req.session.user.email })
+        res.status(200).json({
+            err_code: 200,
+            message: 'OK'
+        })
         req.session.user = null
     } catch (e) {
-        throw e
+        return res.status(500).json({
+            err_code: 500,
+            message: e.message
+        })
     }
-
-
 })
 
 module.exports = router
